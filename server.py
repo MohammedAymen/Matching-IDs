f495401cb7eab0bbf8247e26d23852bd52d1650c
Ahmed Elsewedy <113258942+Ahmed-Elsewedy@users.noreply.github.com>
2024-06-21 01:58:44 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,34 +1,35 @@
 from flask import Flask, request
 import numpy as np
 import cv2
-from main import Comp 
+from main import Comp
 from CompareFace import CompareFace
+
 app = Flask(__name__)
- 
- 
-@app.route("/", methods=["POST"])
+
+@app.route("/", methods=["GET", "POST"])
 def receive_images():
-    images = []
-    for image_file in request.files.getlist("images"):
- 
-        image_bytes = image_file.read()
-        nparr = np.frombuffer(image_bytes, np.uint8)
-        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
- 
-        images.append(img)
-       # print(img)
-        print("image shape",img.shape)
-    #["app", "personal", "system"]
-    imageapp=images[0]
-    imagePers=images[1]
-    imageSystem=images[2]
-    comp_instance = Comp(imageapp, imageSystem)
-    result1= comp_instance.Check()
-    result2 =CompareFace(imageSystem, imagePers)
+    if request.method == "GET":
+        return "Server is live", 200
     
-    if result1 and result2:
-        return "Allowed to register", 200
-    return "Not allowed to register", 400
- 
+    if request.method == "POST":
+        images = []
+        for image_file in request.files.getlist("images"):
+            image_bytes = image_file.read()
+            nparr = np.frombuffer(image_bytes, np.uint8)
+            img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+            images.append(img)
+            print("image shape", img.shape)
+        
+        imageapp = images[0]
+        imagePers = images[1]
+        imageSystem = images[2]
+        comp_instance = Comp(imageapp, imageSystem)
+        result1 = comp_instance.Check()
+        result2 = CompareFace(imageSystem, imagePers)
+        
+        if result1 and result2:
+            return "Allowed to register", 200
+        return "Not allowed to register", 400
+
 if __name__ == "__main__":
     app.run(debug=False)
